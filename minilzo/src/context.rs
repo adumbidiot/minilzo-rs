795025d62446891c09be2e9912c5f01056093d8a
Nathaniel Daniel <nathaniel.daniel23@outlook.com>
2023-01-03 19:47:14 -0800

    Improve library safety (#3)
    * Modularize lib
    
    * Fix test
    
    * Use safer decompression c func
    
    * Add note that context is cheap to copy/clone
    
    * Add Display for ErrorCode
    
    * Make ErrorCode an Error
    
    * Add OutputBuffer type
    
    Functions can accept MaybeUnit<u8> or u8 buffers with no unsafe casting
    
    * Return slice instead of initialized len
    
    * Fix lifetimes
    
    * Add util func to convert from ErrorCode to result

---
+++ minilzo/src/context.rs
@@ -0,0 +1,111 @@
+use crate::ErrorCode;
+use crate::OutputBuffer;
+use minilzo_sys::lzo_align_t;
+use once_cell::sync::Lazy;
+use std::ffi::CStr;
+use std::marker::PhantomData;
+use std::mem::MaybeUninit;
+
+static INIT_ERROR_CODE: Lazy<ErrorCode> =
+    Lazy::new(|| ErrorCode(unsafe { minilzo_sys::lzo_init_func() }));
+
+/// A compression and decompression context.
+///
+/// Serves as proof of library initialization.
+/// This may be copied and cloned freely and cheaply; this is a zero-sized struct.
+///
+/// Note that creating a context incurs some cost,
+/// as if it is the first context the library initialization routines are run.
+/// Additionally,
+/// there is synchronization performed to ensure that the initialization routine is run only once.
+#[derive(Debug, Copy, Clone)]
+pub struct Context {
+    _data: PhantomData<bool>,
+}
+
+impl Context {
+    /// Make a new [`Context`].
+    pub fn new() -> Result<Self, ErrorCode> {
+        INIT_ERROR_CODE.into_result()?;
+
+        Ok(Self { _data: PhantomData })
+    }
+
+    /// Get the version
+    pub fn version(&self) -> u32 {
+        unsafe { minilzo_sys::lzo_version() }
+    }
+
+    /// Get the version string
+    pub fn version_string(&self) -> &'static CStr {
+        unsafe { CStr::from_ptr(minilzo_sys::lzo_version_string().cast()) }
+    }
+
+    /// Get the version date
+    pub fn version_date(&self) -> &'static CStr {
+        unsafe { CStr::from_ptr(minilzo_sys::lzo_version_date().cast()) }
+    }
+
+    /// Compress
+    pub fn compress<'i, 'o>(
+        &self,
+        input: &'i [u8],
+        mut output: impl OutputBuffer + 'o,
+    ) -> Result<&'o mut [u8], ErrorCode> {
+        const WORKSPACE_LEN_BYTES: usize = minilzo_sys::LZO1X_1_MEM_COMPRESS_ as usize;
+        const WORKSPACE_LEN: usize = (WORKSPACE_LEN_BYTES + (std::mem::size_of::<lzo_align_t>())
+            - 1)
+            / std::mem::size_of::<lzo_align_t>();
+
+        let input_len = input.len().try_into().unwrap();
+        let output_ptr = output.get_ptr();
+        let mut output_len = output.get_size();
+        let mut workspace = [MaybeUninit::<lzo_align_t>::uninit(); WORKSPACE_LEN];
+        let error = unsafe {
+            minilzo_sys::lzo1x_1_compress(
+                input.as_ptr(),
+                input_len,
+                output_ptr.cast(),
+                &mut output_len,
+                workspace.as_mut_ptr().cast(),
+            )
+        };
+
+        ErrorCode(error).into_result()?;
+
+        let output_len = output_len.try_into().unwrap();
+        Ok(unsafe { std::slice::from_raw_parts_mut(output_ptr.cast(), output_len) })
+    }
+
+    /// Decompress
+    pub fn decompress<'i, 'o>(
+        &self,
+        input: &'i [u8],
+        mut output: impl OutputBuffer + 'o,
+    ) -> Result<&'o mut [u8], ErrorCode> {
+        let input_len = input.len().try_into().unwrap();
+        let output_ptr = output.get_ptr();
+        let mut output_len = output.get_size();
+
+        let error = unsafe {
+            minilzo_sys::lzo1x_decompress_safe(
+                input.as_ptr(),
+                input_len,
+                output_ptr.cast(),
+                &mut output_len,
+                std::ptr::null_mut(),
+            )
+        };
+
+        ErrorCode(error).into_result()?;
+
+        let output_len = output_len.try_into().unwrap();
+        Ok(unsafe { std::slice::from_raw_parts_mut(output_ptr.cast(), output_len) })
+    }
+}
+
+impl Default for Context {
+    fn default() -> Self {
+        Self::new().expect("failed to init minilzo")
+    }
+}
--- minilzo/src/lib.rs
+++ minilzo/src/lib.rs
@@ -1,121 +1,60 @@
-pub use minilzo_sys::lzo_align_t;
-use once_cell::sync::Lazy;
-use std::ffi::CStr;
-use std::marker::PhantomData;
+mod context;
+mod error_code;
+
+pub use self::context::Context;
+pub use self::error_code::ErrorCode;
 use std::mem::MaybeUninit;
-use std::os::raw::c_int;
 
-static INIT_ERROR_CODE: Lazy<ErrorCode> =
-    Lazy::new(|| ErrorCode(unsafe { minilzo_sys::lzo_init_func() }));
+/// # Safety
+///
+/// * The `as_ptr` function MUST return a valid, mutable pointer to potentially uninitialized memory.
+/// * The `len` function MUST return the length of the section of uninitialized memory returned by `as_ptr`, in bytes.
+#[allow(clippy::len_without_is_empty)]
+pub unsafe trait OutputBuffer {
+    /// Get the ptr.
+    fn get_ptr(&mut self) -> *mut MaybeUninit<u8>;
 
-/// A library error code
-#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
-pub struct ErrorCode(pub c_int);
+    /// Get the length of the buffer.
+    fn get_size(&self) -> u64;
+}
 
-impl ErrorCode {
-    /// No error
-    const OK: Self = ErrorCode(minilzo_sys::LZO_E_OK as c_int);
+unsafe impl OutputBuffer for &mut [u8] {
+    fn get_ptr(&mut self) -> *mut MaybeUninit<u8> {
+        <[u8]>::as_mut_ptr(self).cast()
+    }
 
-    /// Returns true if this is OK
-    pub fn is_ok(self) -> bool {
-        self == Self::OK
+    fn get_size(&self) -> u64 {
+        <[u8]>::len(self).try_into().unwrap()
     }
 }
 
-#[derive(Debug, Clone)]
-pub struct Context {
-    _data: PhantomData<bool>,
-}
-
-impl Context {
-    /// Make a new [`Context`].
-    pub fn new() -> Result<Self, ErrorCode> {
-        let error = *INIT_ERROR_CODE;
-        if !error.is_ok() {
-            return Err(error);
-        }
-
-        Ok(Self { _data: PhantomData })
+unsafe impl OutputBuffer for &mut [MaybeUninit<u8>] {
+    fn get_ptr(&mut self) -> *mut MaybeUninit<u8> {
+        <[MaybeUninit<u8>]>::as_mut_ptr(self).cast()
     }
 
-    /// Get the version
-    pub fn version(&self) -> u32 {
-        unsafe { minilzo_sys::lzo_version() }
-    }
-
-    /// Get the version string
-    pub fn version_string(&self) -> &'static CStr {
-        unsafe { CStr::from_ptr(minilzo_sys::lzo_version_string().cast()) }
-    }
-
-    /// Get the version date
-    pub fn version_date(&self) -> &'static CStr {
-        unsafe { CStr::from_ptr(minilzo_sys::lzo_version_date().cast()) }
-    }
-
-    /// Compress
-    pub fn compress(
-        &self,
-        input: &[u8],
-        output: &mut [MaybeUninit<u8>],
-    ) -> Result<usize, ErrorCode> {
-        const WORKSPACE_LEN_BYTES: usize = minilzo_sys::LZO1X_1_MEM_COMPRESS_ as usize;
-        const WORKSPACE_LEN: usize = (WORKSPACE_LEN_BYTES + (std::mem::size_of::<lzo_align_t>())
-            - 1)
-            / std::mem::size_of::<lzo_align_t>();
-
-        let input_len = input.len().try_into().unwrap();
-        let mut output_len = output.len().try_into().unwrap();
-        let mut workspace = [MaybeUninit::<lzo_align_t>::uninit(); WORKSPACE_LEN];
-        let error = unsafe {
-            minilzo_sys::lzo1x_1_compress(
-                input.as_ptr(),
-                input_len,
-                output.as_mut_ptr().cast(),
-                &mut output_len,
-                workspace.as_mut_ptr().cast(),
-            )
-        };
-
-        let error = ErrorCode(error);
-        if !error.is_ok() {
-            return Err(error);
-        }
-
-        Ok(output_len.try_into().unwrap())
-    }
-
-    /// Decompress
-    pub fn decompress(
-        &self,
-        input: &[u8],
-        output: &mut [MaybeUninit<u8>],
-    ) -> Result<usize, ErrorCode> {
-        let input_len = input.len().try_into().unwrap();
-        let mut output_len = output.len().try_into().unwrap();
-
-        let error = unsafe {
-            minilzo_sys::lzo1x_decompress(
-                input.as_ptr(),
-                input_len,
-                output.as_mut_ptr().cast(),
-                &mut output_len,
-                std::ptr::null_mut(),
-            )
-        };
-
-        let error = ErrorCode(error);
-        if !error.is_ok() {
-            return Err(error);
-        }
-
-        Ok(output_len.try_into().unwrap())
+    fn get_size(&self) -> u64 {
+        <[MaybeUninit<u8>]>::len(self).try_into().unwrap()
     }
 }
 
-impl Default for Context {
-    fn default() -> Self {
-        Self::new().expect("failed to init minilzo")
+unsafe impl OutputBuffer for &mut Vec<u8> {
+    fn get_ptr(&mut self) -> *mut MaybeUninit<u8> {
+        <Vec<u8>>::as_mut_ptr(self).cast()
+    }
+
+    fn get_size(&self) -> u64 {
+        <Vec<u8>>::len(self).try_into().unwrap()
+    }
+}
+
+unsafe impl OutputBuffer for &mut Vec<MaybeUninit<u8>> {
+    fn get_ptr(&mut self) -> *mut MaybeUninit<u8> {
+        <Vec<MaybeUninit<u8>>>::as_mut_ptr(self).cast()
+    }
+
+    fn get_size(&self) -> u64 {
+        <Vec<MaybeUninit<u8>>>::len(self).try_into().unwrap()
     }
 }
 
@@ -133,18 +72,14 @@ mod test {
         let input: Vec<u8> = vec![0; 128 * 1024];
         let compress_output_len = input.len() + input.len() / 16 + 64 + 3;
         let mut compress_output = vec![MaybeUninit::uninit(); compress_output_len];
-        let compressed_len = context
+        let compressed = context
             .compress(&input, &mut compress_output)
             .expect("failed to compress");
-        let compressed: &[u8] =
-            unsafe { std::slice::from_raw_parts(compress_output.as_ptr().cast(), compressed_len) };
 
         let mut decompressed = vec![std::mem::MaybeUninit::uninit(); 128 * 1024];
-        let decompressed_len = context
+        let decompressed = context
             .decompress(compressed, &mut decompressed)
             .expect("failed to decompress");
-        let decompressed: &[u8] =
-            unsafe { std::slice::from_raw_parts(decompressed.as_ptr().cast(), decompressed_len) };
         assert!(decompressed == input);
     }
 }
