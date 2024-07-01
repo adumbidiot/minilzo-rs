4bfc45631f56ee763fa0a6f96f9b120a98bae933
adumbidiot <adumbidiot@users.noreply.github.com>
2024-07-01 23:02:50 +0000

    deploy: d7a782f45176cb6b3fbd6c44934fd8a71b8a3c8a

---
+++ crates.js
@@ -0,0 +1,1 @@
+window.ALL_CRATES = ["minilzo","minilzo_sys"];
---
+++ src-files.js
@@ -0,0 +1,5 @@
+var srcIndex = new Map(JSON.parse('[\
+["minilzo",["",[],["context.rs","error_code.rs","lib.rs"]]],\
+["minilzo_sys",["",[],["lib.rs"]]]\
+]'));
+createSrcSidebar();
